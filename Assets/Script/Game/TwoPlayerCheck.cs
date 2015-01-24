5569db08908e334fc4c5130291fe540179a466ea
Julian Löhr <julian.loehr@outlook.com>
2015-01-24 13:57:13 +0100

    Added splitscreen for two players

---
+++ Assets/Script/Game/TwoPlayerCheck.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TwoPlayerCheck : MonoBehaviour {
+
+    public SkeletonWrapper KinectSkeleton;
+
+    public Camera PlayerOne;
+    public Camera PlayerTwo;
+
+    bool SinglePlayer;
+
+    void Start()
+    {
+        SinglePlayer = true;
+    }
+
+    void Update()
+    {
+        if(KinectSkeleton.pollSkeleton())
+        {
+            bool TwoSkeletons = (KinectSkeleton.trackedPlayers[1] >= 0);
+            
+            if(SinglePlayer && TwoSkeletons)
+            {
+                ChangeToSplitscreen();
+            }
+
+            if(!SinglePlayer && !TwoSkeletons)
+            {
+                ChangeToFullscreen();
+            }
+        }
+    }
+
+    void ChangeToSplitscreen()
+    {
+        SinglePlayer = false;
+
+        Rect PlayerOneRect = PlayerOne.rect;
+        PlayerOneRect.width = 0.5f;
+        PlayerOne.rect = PlayerOneRect;
+
+        PlayerTwo.enabled = true;
+    }
+
+    void ChangeToFullscreen()
+    {
+        SinglePlayer = true;
+
+        Rect PlayerOneRect = PlayerOne.rect;
+        PlayerOneRect.width = 1f;
+        PlayerOne.rect = PlayerOneRect;
+
+        PlayerTwo.enabled = false;
+    }
+}
