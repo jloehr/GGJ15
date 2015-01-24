2d1444a00b0f666625deca244ad6c7a463b6d9e5
s0547174 <s047174@htw-berlin.de>
2015-01-24 15:17:48 +0100

    add save or unsave Mode

---
+++ Assets/Script/Logic/armCollision.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class armCollision : MonoBehaviour {
+
+    public checkState tetst;
+    void OnCollisionEnter()
+    {
+        if (tetst.save == false) { Debug.Log("erwischt"); }
+        Debug.Log("Hit");
+    }
+}
---
+++ Assets/Script/Logic/checkState.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class checkState : MonoBehaviour {
+
+    public bool save;
+    public bool getCaught;
+
+	void Start () {
+        save = true;
+        getCaught = false;
+        Invoke("SetUnsave", 30);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    void SetUnsave()
+    {
+        Debug.Log("unsicher");
+        save = false;
+    }
+
+}
