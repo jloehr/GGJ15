cba33603d77d720101fc4a2ef557541f191c09e9
Julian Löhr <julian.loehr@outlook.com>
2015-01-24 19:53:54 +0100

    Fixed Points

--- Assets/Script/Game/getPoints.cs
+++ Assets/Script/Game/getPoints.cs
@@ -5,24 +5,20 @@ public class getPoints : MonoBehaviour {
 
     public checkState checkActualState;
 	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 
     void OnCollisionEnter(Collision col)
     {
         if ((col.gameObject.tag != "Baby") && (checkActualState.save == true))
         {
-            Player player = GetComponent<Player>();
-            if(player != null)
+            CheckPush Push = GetComponent<CheckPush>();
+            if(Push != null)
             {
-                player.Points += 10;
-                Debug.Log(player.Points);
+                Player player = Push.player;
+                if (player != null)
+                {
+                    player.Points += 10;
+                    Debug.Log(player.Points);
+                }
             }
         }
     }
