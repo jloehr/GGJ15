cc8826f8d69ec835e97d2364b84fd4d2d16d3d95
s0547174 <s047174@htw-berlin.de>
2015-01-24 18:41:49 +0100

    add PointCOntroll

---
+++ Assets/Script/Game/getPoints.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class getPoints : MonoBehaviour {
+
+    public checkState checkActualState;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter(Collision col)
+    {
+        if ((col.gameObject.tag != "Baby") && (checkActualState.save == true))
+        {
+            Debug.Log("Punkt erhalten");
+        }
+    }
+}
