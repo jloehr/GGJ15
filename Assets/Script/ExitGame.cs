bff7b377ff308e4b0bdc3a45cb2ad60ebfd397f4
Julian Löhr <julian.loehr@outlook.com>
2015-01-25 15:08:27 +0100

    Added Color to Objects, Painting and Splash Screen

--- Assets/Script/ExitGame.cs
+++ Assets/Script/ExitGame.cs
@@ -3,13 +3,12 @@ using System.Collections;
 
 public class ExitGame : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+	    if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
 	}
 }
